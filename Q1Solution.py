514955301debb773eebb723c9c2e90d84c21257c
vanesaakirezi <vanessakirezi4@gmail.com>
2024-12-18 21:07:28 +0200

    Submit
    Updated changes

--- Q1Solution.py
+++ Q1Solution.py
@@ -5,3 +5,20 @@ def findRent(start,end):
 start=int(input("Enter start time"))
 end=int(input("Enter end time"))
 print(findRent(start,end))
+#Write a python program that ask the user to input apositive integer
+#and print all prime numbers less than or equal to the input .
+#only accept positive integer as inputs.
+""""""
+def is_prime(num):
+ if num <1:
+    return False
+ for i in range (2, int(num/2 + 1)):
+     if num % i == 0:
+        return False
+ return true
+def print_prime(n):
+      for num in range(2,n+1):
+         if is_prime(num):
+            print(num)
+
+  
