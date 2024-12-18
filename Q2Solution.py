93a090cac75e15d74a9be3a1ff55315635d955b7
vanesaakirezi <vanessakirezi4@gmail.com>
2024-12-18 21:10:44 +0200

    Update Q2Solution.py

--- Q2Solution.py
+++ Q2Solution.py
@@ -1,1 +1,18 @@
 #Solution to question 2 goes here
+#Write a python program that ask the user to input apositive integer
+#and print all prime numbers less than or equal to the input .
+#only accept positive integer as inputs.
+""""""
+def is_prime(num):
+ if num <1:
+    return False
+ for i in range (2, int(num/2 + 1)):
+     if num % i == 0:
+        return False
+ return true
+def print_prime(n):
+      for num in range(2,n+1):
+         if is_prime(num):
+            print(num)
+
+  
